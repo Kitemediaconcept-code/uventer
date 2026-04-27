ad4d4996e7eb18f72bf1f62994fd723c59f937f3
ajmaloffical04-design <ajmaloffical04@gmail.com>
2026-04-27 13:05:46 +0530

    style: remove border from featured card

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -56,7 +56,7 @@ const Hero = () => {
           initial={{ opacity: 0, x: 20 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.8, delay: 0.1, ease: [0.16, 1, 0.3, 1] }}
-          className="relative bg-black rounded-[35px] overflow-hidden flex flex-col min-h-[450px] lg:min-h-[600px] group border border-black"
+          className="relative bg-black rounded-[35px] overflow-hidden flex flex-col min-h-[450px] lg:min-h-[600px] group"
         >
           {/* Top Section - Image Background */}
           <div className="relative h-[45%] w-full overflow-hidden">
